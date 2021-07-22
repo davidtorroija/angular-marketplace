c10b67daa33604ca630288c304a2f9f5825d1ea3
Diego Herrera <diego@Diegos-MacBook-Pro.local>
2021-07-22 09:19:28 -0500

    AMP-19 drop layoutmodule declaration on app.module, since is used with lazy loading

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,11 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LayoutModule } from './layout/layout.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, LayoutModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
