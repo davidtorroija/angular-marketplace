b18f87cda285f939af5466ec8bfccabe1bc9aa1e
Jeannuel Garcia <jjeanlink@gmail.com>
2021-07-23 10:35:21 -0500

    Feat: Importing Shared Module to Auth Module

--- client/src/app/auth/auth.module.ts
+++ client/src/app/auth/auth.module.ts
@@ -1,17 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from './../shared/shared.module';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './pages/login/login.component';
 
-
 @NgModule({
-  declarations: [
-    LoginComponent
-  ],
-  imports: [
-    CommonModule,
-    AuthRoutingModule
-  ]
+  declarations: [LoginComponent],
+  imports: [CommonModule, AuthRoutingModule, SharedModule]
 })
-export class AuthModule { }
+export class AuthModule {}
--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -3,12 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { SharedRoutingModule } from './shared-routing.module';
 
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    SharedRoutingModule
-  ]
+  imports: [CommonModule, SharedRoutingModule],
+  exports: [MatInputModule]
 })
-export class SharedModule { }
+export class SharedModule {}
