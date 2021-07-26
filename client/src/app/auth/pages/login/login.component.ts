6220bd93a244e8039b2558ba546f66947450e5cd
Jeannuel Garcia <jjeanlink@gmail.com>
2021-07-26 16:45:51 -0500

    Feat: Login view. Added scss folder for scss imports. Utility classes.
    - angular.json
    Added stylePreprocessorOptions to include the scss folder which will contain scss files that we will call if needed with only "@import 'nameofthefile';"
    
    - styles.scss
    Added some utility classes.
    
    - /auth/pages/login
    Added the view for the login with validators.

--- client/src/app/auth/pages/login/login.component.ts
+++ client/src/app/auth/pages/login/login.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-login',
@@ -6,10 +16,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  matcher = new MyErrorStateMatcher();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private formbuilder: FormBuilder) {
+    this.loginForm = this.initLoginForm();
   }
 
+  ngOnInit(): void {}
+
+  private initLoginForm() {
+    return this.formbuilder.group({
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(8)])
+    });
+  }
+
+  get f() {
+    return this.loginForm.controls;
+  }
+
+  onSubmit() {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+    } else {
+      // HTTP Request
+      alert(JSON.stringify(this.loginForm.value));
+    }
+  }
 }
--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { SharedRoutingModule } from './shared-routing.module';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+// Material
 import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule, SharedRoutingModule],
-  exports: [MatInputModule]
+  exports: [FormsModule, ReactiveFormsModule, MatInputModule, MatButtonModule, MatCardModule]
 })
 export class SharedModule {}
