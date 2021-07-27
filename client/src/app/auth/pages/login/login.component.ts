6b29c3521e25dedb2e84ab830d1a373ed13fd3f1
Jeannuel Garcia <jjeanlink@gmail.com>
2021-07-27 10:45:23 -0500

    Fix: Moving state error matcher to shared/errors

--- client/src/app/auth/pages/login/login.component.ts
+++ client/src/app/auth/pages/login/login.component.ts
@@ -1,14 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, FormGroupDirective, NgForm, Validators } from '@angular/forms';
-import { ErrorStateMatcher } from '@angular/material/core';
-
-/** Error when invalid control is dirty, touched, or submitted. */
-export class MyErrorStateMatcher implements ErrorStateMatcher {
-  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
-    const isSubmitted = form && form.submitted;
-    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
-  }
-}
+import { MyErrorStateMatcher } from './../../../shared/errors/errorStateMatcher';
 
 @Component({
   selector: 'app-login',
